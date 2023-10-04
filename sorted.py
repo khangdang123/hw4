5a20cbb4d84f300f5852821e35e9bd5dab904558
khangdang123 <84164518+khangdang123@users.noreply.github.com>
2023-10-03 17:41:42 -0700

    Create sorted.py

---
+++ sorted.py
@@ -0,0 +1,8 @@
+def sort_dictionary(dict):
+    sorted_list = []
+
+    for name, (phone_number, age) in sorted_list(dict.items(), key = lambda x: x[1][1]):
+
+      sorted_list.append((name, phone_number))
+
+      return sorted_list
