e857f6843ae943043f33b105a6711728a11b3519
khangdang123 <84164518+khangdang123@users.noreply.github.com>
2023-10-04 17:11:10 -0700

    Update bug1.py

--- bug1.py
+++ bug1.py
@@ -1,1 +1,22 @@
-BIG CIRCLE
+class Base:
+# TODO: there's code missing in one or more lines below
+
+class Circle():
+    def __init__(x, size):
+        super().__init__(x, y, size)
+
+    def draw(self):
+        return f"""
+({self.x}, {self.y})\n{self.size}
+         , - ~ ~ ~ - ,
+     , '               ' ,
+   ,                       ,
+  ,                         ,
+ ,                           ,
+ ,                           ,
+ ,                           ,
+  ,                         ,
+   ,                       ,
+     ,                  , '
+       ' - , _ _ _ ,  '
+               """
