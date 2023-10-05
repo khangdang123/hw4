753564ba8644e471b16a66b20d937889d5caf00f
khangdang123 <84164518+khangdang123@users.noreply.github.com>
2023-10-04 17:19:15 -0700

    Update bug1.py

--- bug1.py
+++ bug1.py
@@ -1,8 +1,11 @@
 class Base:
-# TODO: there's code missing in one or more lines below
+    def __init__(self,x,y,size):
+        self.x = x
+        self.y = y
+        self.size = size
 
-class Circle():
-    def __init__(x, size):
+class Circle(Base):
+    def __init__(self, x,y,size):
         super().__init__(x, y, size)
 
     def draw(self):
@@ -20,3 +23,10 @@ class Circle():
      ,                  , '
        ' - , _ _ _ ,  '
                """
+
+
+def main():
+    c = Circle(1,2,3)
+    print(c.draw())
+    
+main()
