4dddeda99c52d4541acc307a776d034c37e5387d
khangdang123 <84164518+khangdang123@users.noreply.github.com>
2023-10-07 22:47:02 -0700

    Update ladder.py

--- ladder.py
+++ ladder.py
@@ -2,7 +2,7 @@ def my_steps(n):
   if n < 1 or n > 25:
         raise Error("Out of bounds.")
     
-  def count_ways(s):
+  def count_ways(n):
     if n <= 1:
       return 1
     return count_ways(n-1) + count_ways(n-2)
