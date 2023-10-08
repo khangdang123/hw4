003fd23a01e64b14a35605e637cd7439a80b0f83
khangdang123 <84164518+khangdang123@users.noreply.github.com>
2023-10-07 22:54:53 -0700

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,13 +1,9 @@
 def merge_list(list1, list2):
     mergeList = list1 + list2
-    swapped = False
-    for i in range(len(mergeList) - 1, 0, -1):    
-        for j in range(i):  
+    
+    for i in range(len(mergeList) - 1):    
+        for j in range(0, len(mergeList) - i - 1):  
             if mergeList[j] > mergeList[j+1]:   
                 mergeList[j], mergeList[j+1] = mergeList[j+1], mergeList[j]
-                swapped = True  
-        if swapped:
-            swapped = False
-        else:
-            break
+
     return mergeList
