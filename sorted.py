cfc988825226ef63c8a15e12a50b84d5905b8d20
khangdang123 <84164518+khangdang123@users.noreply.github.com>
2023-10-09 09:19:56 -0700

    Update sorted.py

--- sorted.py
+++ sorted.py
@@ -1,7 +1,7 @@
 def sort_dictionary(d):
     sorted_list = []
 
-    for name, (phone_number, age) in sorted(d.items(), key=lambda x: x[1][1],reverse = True):
+    for name, (phone_number, age) in sorted(d.items(), key=lambda x: x[1][1]):
         sorted_list.append((name, phone_number))
 
     return sorted_list
